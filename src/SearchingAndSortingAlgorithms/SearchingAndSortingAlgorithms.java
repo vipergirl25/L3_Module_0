2aa892fa95c2f9ed07795a9e417d870afba641b7
DaveDLeague <dave.dunn@jointheleague.org>
2017-04-11 13:37:19 -0700

    Update SearchingAndSortingAlgorithms.java

--- src/SearchingAndSortingAlgorithms/SearchingAndSortingAlgorithms.java
+++ src/SearchingAndSortingAlgorithms/SearchingAndSortingAlgorithms.java
@@ -67,9 +67,9 @@ public class SearchingAndSortingAlgorithms {
 //	public void testSort() throws Exception {
 //		List<Double> results = Arrays.asList(new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1,
 //				66.6 });
-//		assertEquals(38.4, Algorithms.sortScores(results).get(0));
-//		assertEquals(40.7, Algorithms.sortScores(results).get(1));
-//		assertEquals(95.0, Algorithms.sortScores(results).get(13));
+//		assertEquals(38.4, Algorithms.sortScores(results).get(0), 0.0);
+//		assertEquals(40.7, Algorithms.sortScores(results).get(1), 0.0);
+//		assertEquals(95.0, Algorithms.sortScores(results).get(13), 0.0);
 //	}
 
 	/* 8. Sort the DNA sequences by length from shortest to longest. */
